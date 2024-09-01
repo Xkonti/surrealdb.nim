77372e281a57cdd3cef111611f0cf8e1ad9c0b35
Xkonti <xkonti@xkonti.tech>
2024-09-01 00:06:20 -0400

    Make things fully async

--- src/surreal.nim
+++ src/surreal.nim
@@ -1,16 +1,48 @@
 # This is just an example to get you started. A typical binary package
 # uses this file as the main entry point of the application.
 
-# import asyncdispatch, pkg/websocket
-import std/strutils
-import asyncdispatch, ws
+import std/[asyncdispatch, asyncfutures, json, sequtils, strutils, tables]
+import ws
 
-proc receiveResponse(ws: WebSocket, id: int): Future[string] {.async.} =
-  let idStr = """"id":""" & $id & ","
+var nextId = 0
+
+proc getNextId(): int =
+  inc(nextId)
+  return nextId
+
+var queryFutures = newTable[int, Future[JsonNode]]()
+
+
+proc startListenLoop(ws: WebSocket) {.async.} =
+  echo "Starting listen loop"
   while true:
     var resp = await ws.receivePacket()
-    if resp[0] == Opcode.Text and resp[1].contains(idStr):
-      return resp[1]
+    if resp[0] != Opcode.Text:
+      continue
+    let jsonObject = parseJson(resp[1])
+    let queryId: int = jsonObject["id"].getInt()
+    echo "Reqponse for query ID: ", queryId
+    if queryFutures.hasKey(queryId):
+      let future = queryFutures[queryId]
+      queryFutures.del(queryId)
+      future.complete(jsonObject)
+
+
+proc sendQuery(ws: WebSocket, query: string): Future[JsonNode] {.async.} =
+  let queryId = getNextId()
+  echo "Sending query with ID: ", queryId
+  let future: Future[JsonNode] = newFuture[JsonNode]("sendQuery #" & $queryId)
+
+  try:
+    let queryWithId = query.replace("\"id\":-1", "\"id\":" & $queryId)
+    await ws.send(queryWithId)
+    queryFutures[queryId] = future
+  except CatchableError as e:
+    echo "Error sending query: ", e.msg
+    future.fail(e)
+  finally:
+    return await future
+
 
 proc main() {.async.} =
   # let ws = await newAsyncWebsocketClient("jabba.lan", Port(14831),
@@ -20,20 +52,20 @@ proc main() {.async.} =
   ws.setupPings(15)
   echo "connected!"
 
+  # Start loop that listens for responses from the database
+  asyncCheck startListenLoop(ws)
+
   const useMsg = """{
-    "id": 1,
+    "id":-1,
     "method": "use",
     "params": [ "test", "test" ]
   }"""
   
-  await ws.send(useMsg)
-  echo "Sent `use` message"
-
-  var resp = await ws.receiveResponse(1)
+  var resp = await ws.sendQuery(useMsg)
   echo "Received response: ", resp
 
   const signInMsg = """{
-    "id": 2,
+    "id":-1,
     "method": "signin",
     "params": [
         {
@@ -43,21 +75,22 @@ proc main() {.async.} =
     ]
   }"""
 
-  await ws.send(signInMsg)
-  echo "Sent `signin` message"
-  resp = await ws.receiveResponse(2)
+  resp = await ws.sendQuery(signInMsg)
   echo "Received response: ", resp
 
   const selectMsg = """{
-    "id": 3,
+    "id":-1,
     "method": "select",
     "params": [ "item" ]
   }"""
 
-  await ws.send(selectMsg)
-  echo "Sent `select` message"
-  resp = await ws.receiveResponse(3)
-  echo "Received response: ", resp
+  var futures: seq[Future[JsonNode]] = @[]
+  for i in 0..<50:
+    futures.add(ws.sendQuery(selectMsg))
+
+  let responses = await futures.all()
+
+  echo "Received ", responses.len, " responses"
 
   ws.close()
 

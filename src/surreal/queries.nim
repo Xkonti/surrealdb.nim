59bb3ce56a964bf68a416c1c46937807bdfc1b5c
Xkonti <xkonti@xkonti.tech>
2024-09-14 01:04:35 -0400

    Add `select` query

--- src/surreal/queries.nim
+++ src/surreal/queries.nim
@@ -14,8 +14,4 @@ include ./private/query_unset
 # TODO: ./private/query_live
 # TODO: ./private/query_kill
 include ./private/query_query
-
-proc select*(db: SurrealDB, thing: string): Future[SurrealResult[JsonNode]] {.async.} =
-    # TODO: Adhere to the RPC docs
-    let params = %* [ thing ]
-    return await db.sendQuery(RpcMethod.Select, params)
+include ./private/query_select

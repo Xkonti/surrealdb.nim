e4a689bda7b140737afad992e3041c417fb4201b
Xkonti <xkonti@xkonti.tech>
2024-09-20 02:53:21 -0400

    Potential list of surreal types

---
+++ src/surreal/private/types/surrealTypes.nim
@@ -0,0 +1,24 @@
+type SurrealTypes* = enum
+    ## Supported Surreal types
+
+    SurrealNone,
+    SurrealNull,
+
+    SurrealArray,
+    SurrealBool,
+    SurrealBytes,
+    SurrealDatetime,
+    SurrealDecimal,
+    SurrealDuration,
+    SurrealFloat,
+    SurrealGeometry,
+    SurrealInt,
+    SurrealNumber,
+    SurrealObject,
+    SurrealLiteral,
+    SurrealOption,
+    SurrealRange,
+    SurrealRecord,
+    SurrealSet,
+    SurrealString,
+    SurrealUuid,

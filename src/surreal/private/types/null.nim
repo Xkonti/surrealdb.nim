f2aac7a88671ba1e0e31c51cc3b414ca6230976e
Xkonti <xkonti@xkonti.tech>
2024-09-22 14:20:24 -0400

    Fix the null type

--- src/surreal/private/types/null.nim
+++ src/surreal/private/types/null.nim
@@ -5,6 +5,6 @@ type
 const Null* : NullType = NullType()
     ## A Surreal NULL value
 
-proc `$`*(n: Null): string =
+proc `$`*(n: NullType): string =
     ## Returns the string representation of the `Null` object.
     "null"
